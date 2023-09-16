4bebc84c667496733aea3c2d55492a123806e9f4
Devansh-365 <devanshtiwari365@gmail.com>
2023-09-17 01:09:26 +0530

    feat: add dashboard layout

---
+++ src/app/(protected)/dashboard/layout.tsx
@@ -0,0 +1,22 @@
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+import React from "react";
+
+interface DashboardLayoutProps {
+  children?: React.ReactNode;
+}
+
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <div className="flex min-h-screen flex-col space-y-6">
+      <Navbar />
+      <div className="container grid flex-1 gap-12 pt-24 md:grid-cols-[200px_1fr]">
+        <aside className="hidden w-[200px] flex-col md:flex"></aside>
+        <main className="flex w-full flex-1 flex-col overflow-hidden">
+          {children}
+        </main>
+      </div>
+      <Footer />
+    </div>
+  );
+}
