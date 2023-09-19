0ed62e41182e6c8ae9525c499c8193bfa6e9554d
Devansh-365 <devanshtiwari365@gmail.com>
2023-09-19 18:06:40 +0530

    feat: update post api

--- src/app/api/posts/[postId]/route.ts
+++ src/app/api/posts/[postId]/route.ts
@@ -1,6 +1,24 @@
 import db from "@/lib/db";
 import { getCurrentUser } from "@/lib/session";
 import { NextResponse } from "next/server";
+import * as z from "zod";
+
+const postSchema = z.object({
+  title: z.string().min(2),
+  description: z.string().min(10),
+  companyName: z.string().min(2),
+  skills: z.array(
+    z.object({
+      id: z.string(),
+      text: z.string(),
+    })
+  ),
+  location: z.string(),
+  country: z.string(),
+  city: z.string(),
+  employmentType: z.string(),
+  opinion: z.string().optional(),
+});
 
 export async function DELETE(
   req: Request,
@@ -24,6 +42,48 @@ export async function DELETE(
     return NextResponse.json(post);
   } catch (error) {
     console.log("[POST_DELETE]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { postId: string } }
+) {
+  try {
+    const user = await getCurrentUser();
+
+    if (!user) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const json = await req.json();
+    const data = postSchema.parse(json);
+
+    const job = await db.job.update({
+      where: {
+        id: params.postId,
+      },
+      data: {
+        title: data.title,
+        description: data.description,
+        companyName: data.companyName,
+        skills: {
+          create: data.skills.map((skill) => ({
+            text: skill.text,
+          })),
+        },
+        location: data.location,
+        country: data.country,
+        city: data.city,
+        employmentType: data.employmentType,
+        opinion: data.opinion,
+      },
+    });
+
+    return NextResponse.json(job);
+  } catch (error) {
+    console.log("[JOBS_PATCH]", error);
     return new NextResponse("Internal error", { status: 500 });
   }
 }
