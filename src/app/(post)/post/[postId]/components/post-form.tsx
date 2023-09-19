3b88262f1b1c6da3a109dab12e95f0b94a5b8e89
Devansh-365 <devanshtiwari365@gmail.com>
2023-09-19 12:44:26 +0530

    feat: add post form

--- src/app/(post)/post/[postId]/components/post-form.tsx
+++ src/app/(post)/post/[postId]/components/post-form.tsx
@@ -7,9 +7,16 @@ import { Icons } from "@/components/icons";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
-import { Trash } from "lucide-react";
+import { CheckIcon, Trash } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
 import {
   Form,
   FormControl,
@@ -19,28 +26,103 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
+import { Tag, TagInput } from "@/components/ui/tag-input";
+import { CaretSortIcon } from "@radix-ui/react-icons";
+import { Textarea } from "@/components/ui/textarea";
+import { City, Country } from "country-state-city";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { redirect, useRouter } from "next/navigation";
+
+const locations = [
+  { label: "Remote work", value: "remote" },
+  { label: "Hybrid Remote Work", value: "hybrid" },
+  { label: "On-Site Work", value: "on-site" },
+] as const;
+
+const employmentTypes = [
+  { label: "Freelance", value: "freelance" },
+  { label: "Contract Employee", value: "contract" },
+  { label: "Permanent Employee", value: "permanent" },
+] as const;
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  title: z.string().min(2, {
+    message: "title must be at least 2 characters.",
   }),
+  description: z.string().min(10, {
+    message: "Job description must be at least 10 characters.",
+  }),
+  companyName: z.string().min(2, {
+    message: "Comapany Name be at least 2 characters.",
+  }),
+  skills: z.array(
+    z.object({
+      id: z.string(),
+      text: z.string(),
+    })
+  ),
+  location: z.string({
+    required_error: "Please select a location.",
+  }),
+  country: z.string({
+    required_error: "Please select a country.",
+  }),
+  city: z.string({
+    required_error: "Please select a city.",
+  }),
+  employmentType: z.string({
+    required_error: "Please select a employment type.",
+  }),
+  opinion: z.string().optional(),
 });
 
 export default function PostForm({ initialData }: any) {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      title: "",
+      description: "",
+      companyName: "",
+      skills: [],
+      location: "",
+      country: "",
+      city: "",
+      employmentType: "",
+      opinion: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
-  }
+  const [tags, setTags] = React.useState<Tag[]>([]);
+
+  const { getValues, setValue } = form;
+
+  const countries = Country.getAllCountries();
+  const [countryCode, setcountryCode] = React.useState("");
+  const states = City.getCitiesOfCountry(countryCode);
+
+  const router = useRouter();
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.post("/api/posts", values);
+
+      form.reset();
+      toast.success(toastMessage);
+      redirect("/dashboard");
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong.");
+    }
+  };
 
   const title = initialData ? "Edit post" : "Create post";
   const description = initialData ? "Edit a post." : "Add a new post";
@@ -54,7 +136,7 @@ export default function PostForm({ initialData }: any) {
           href="/dashboard"
           className={cn(
             buttonVariants({ variant: "ghost" }),
-            "absolute left-4 top-4 md:left-8 md:top-16"
+            "absolute left-4 top-14 md:left-8 md:top-16"
           )}
         >
           <>
@@ -80,22 +162,348 @@ export default function PostForm({ initialData }: any) {
         >
           <FormField
             control={form.control}
-            name="username"
+            name="title"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
+                <FormLabel>Job Title</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input placeholder="Job Title" {...field} />
+                </FormControl>
+                <FormDescription>Enter your job title.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Job Description</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Tell us a little bit about your job"
+                    className="resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>Describe your job.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="companyName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Company Name" {...field} />
+                </FormControl>
+                <FormDescription>Enter your company name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="skills"
+            render={({ field }) => (
+              <FormItem className="flex flex-col items-start">
+                <FormLabel className="text-left">Skills</FormLabel>
+                <FormControl>
+                  <TagInput
+                    {...field}
+                    placeholder="Enter a skill"
+                    tags={tags}
+                    className="min-w-[326px] md:w-[448px]"
+                    setTags={(newTags) => {
+                      setTags(newTags);
+                      setValue("skills", newTags as [Tag, ...Tag[]]);
+                    }}
+                  />
+                </FormControl>
+                <FormDescription>Add your skills.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Location</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        className={cn(
+                          "justify-between rounded-md",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value
+                          ? locations.find(
+                              (location) => location.value === field.value
+                            )?.label
+                          : "Select location"}
+                        <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="min-w-[326px] md:w-[448px] p-0">
+                    <Command>
+                      <CommandInput
+                        placeholder="Search location..."
+                        className="h-9"
+                      />
+                      <CommandEmpty>No location found.</CommandEmpty>
+                      <CommandGroup>
+                        {locations.map((location) => (
+                          <CommandItem
+                            value={location.label}
+                            key={location.value}
+                            onSelect={() => {
+                              form.setValue("location", location.value);
+                            }}
+                          >
+                            {location.label}
+                            <CheckIcon
+                              className={cn(
+                                "ml-auto h-4 w-4",
+                                location.value === field.value
+                                  ? "opacity-100"
+                                  : "opacity-0"
+                              )}
+                            />
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>Select your location.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="country"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Country</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        className={cn(
+                          "justify-between rounded-md",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value
+                          ? countries.find((city) => city.name === field.value)
+                              ?.name
+                          : "Select Country"}
+                        <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="min-w-[326px] md:w-[448px] p-0">
+                    <Command>
+                      <CommandInput
+                        placeholder="Search country..."
+                        className="h-9"
+                      />
+                      <CommandEmpty>No country found.</CommandEmpty>
+                      <CommandGroup className=" overflow-y-scroll max-h-[200px]">
+                        {countries.map((location) => (
+                          <CommandItem
+                            value={location.name}
+                            key={location.name}
+                            onSelect={() => {
+                              setcountryCode(location.isoCode);
+                              form.setValue("country", location.name);
+                            }}
+                          >
+                            {location.name}
+                            <CheckIcon
+                              className={cn(
+                                "ml-auto h-4 w-4",
+                                location.name === field.value
+                                  ? "opacity-100"
+                                  : "opacity-0"
+                              )}
+                            />
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>Choose your country.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>City</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        className={cn(
+                          "justify-between rounded-md",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value
+                          ? states &&
+                            states.find((city) => city.name === field.value)
+                              ?.name
+                          : "Select City"}
+                        <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="min-w-[326px] md:w-[448px] p-0">
+                    <Command>
+                      <CommandInput
+                        placeholder="Search city..."
+                        className="h-9"
+                      />
+                      <CommandEmpty>No city found.</CommandEmpty>
+                      <CommandGroup className=" overflow-y-scroll max-h-[200px]">
+                        {states &&
+                          states.map((location) => (
+                            <CommandItem
+                              value={location.name}
+                              key={location.name}
+                              onSelect={() => {
+                                form.setValue("city", location.name);
+                              }}
+                            >
+                              {location.name}
+                              <CheckIcon
+                                className={cn(
+                                  "ml-auto h-4 w-4",
+                                  location.name === field.value
+                                    ? "opacity-100"
+                                    : "opacity-0"
+                                )}
+                              />
+                            </CommandItem>
+                          ))}
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>Select your city.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="employmentType"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Employment Type</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        className={cn(
+                          "justify-between rounded-md",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value
+                          ? employmentTypes.find(
+                              (employmentType) =>
+                                employmentType.value === field.value
+                            )?.label
+                          : "Select Employment Type"}
+                        <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="min-w-[326px] md:w-[448px] p-0">
+                    <Command>
+                      <CommandInput
+                        placeholder="Search employment type..."
+                        className="h-9"
+                      />
+                      <CommandEmpty>No employment type.</CommandEmpty>
+                      <CommandGroup>
+                        {employmentTypes.map((employmentType) => (
+                          <CommandItem
+                            value={employmentType.label}
+                            key={employmentType.value}
+                            onSelect={() => {
+                              form.setValue(
+                                "employmentType",
+                                employmentType.value
+                              );
+                            }}
+                          >
+                            {employmentType.label}
+                            <CheckIcon
+                              className={cn(
+                                "ml-auto h-4 w-4",
+                                employmentType.value === field.value
+                                  ? "opacity-100"
+                                  : "opacity-0"
+                              )}
+                            />
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>Select your employment type.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="opinion"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Share your opinion</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Tell us a little bit about your job"
+                    className="resize-none"
+                    {...field}
+                  />
                 </FormControl>
                 <FormDescription>
-                  This is your public display name.
+                  Share your opinion about this job (optional).
                 </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
           <Button type="submit" className="w-fit ml-auto">
-            Submit
+            {action}
           </Button>
         </form>
       </Form>
---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,79 @@
+import db from "@/lib/db";
+import { getCurrentUser } from "@/lib/session";
+import { NextResponse } from "next/server";
+import * as z from "zod";
+
+const postSchema = z.object({
+  title: z.string().min(2),
+  description: z.string().min(10),
+  companyName: z.string().min(2),
+  skills: z.array(
+    z.object({
+      id: z.string(),
+      text: z.string(),
+    })
+  ),
+  location: z.string(),
+  country: z.string(),
+  city: z.string(),
+  employmentType: z.string(),
+  opinion: z.string().optional(),
+});
+
+export async function GET(req: Request) {
+  try {
+    const user = await getCurrentUser();
+
+    if (!user) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const jobs = await db.job.findMany({
+      where: {
+        userId: user?.id,
+      },
+    });
+
+    return NextResponse.json(jobs);
+  } catch (error) {
+    console.log("[JOBS_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const user = await getCurrentUser();
+
+    if (!user) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const json = await req.json();
+    const data = postSchema.parse(json);
+
+    const job = await db.job.create({
+      data: {
+        userId: user.id,
+        title: data.title,
+        description: data.description,
+        companyName: data.companyName,
+        skills: {
+          create: data.skills.map((skill) => ({
+            text: skill.text,
+          })),
+        },
+        location: data.location,
+        country: data.country,
+        city: data.city,
+        employmentType: data.employmentType,
+        opinion: data.opinion,
+      },
+    });
+
+    return NextResponse.json(job);
+  } catch (error) {
+    console.log("[JOBS_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
