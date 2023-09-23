03a664a92919cd19f0df047af757ace562c35afe
Devansh-365 <devanshtiwari365@gmail.com>
2023-09-24 00:07:59 +0530

    feat: update settings page

--- src/app/(protected)/dashboard/(routes)/settings/page.tsx
+++ src/app/(protected)/dashboard/(routes)/settings/page.tsx
@@ -1,17 +1,28 @@
 import { Icons } from "@/components/icons";
+import { UserNameForm } from "@/components/user-name-form";
+import { authOptions } from "@/lib/auth";
+import { getCurrentUser } from "@/lib/session";
+import { redirect } from "next/navigation";
 import React from "react";
 
-type Props = {};
+export default async function SettingsPage() {
+  const user = await getCurrentUser();
 
-export default function SettingsPage({}: Props) {
+  if (!user) {
+    redirect(authOptions?.pages?.signIn || "/login");
+  }
+
   return (
     <>
-      <div className="flex items-center justify-between px-2">
+      <div className="flex flex-col px-2">
         <div className="grid gap-1">
           <h1 className="font-semibold text-3xl md:text-4xl">Settings</h1>
           <p className="text-lg text-muted-foreground">
             Manage account and website settings.
           </p>
+        </div>
+        <div className="grid gap-10 mt-8">
+          <UserNameForm user={{ id: user.id, name: user.name || "" }} />
         </div>
       </div>
     </>
